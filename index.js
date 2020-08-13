35d17521c54c0823cb1cd043492fd0b2792821ae
“aritramullick” <aritramullick@g.ucla.edu>
2020-08-13 12:28:00 +0400

    News API Functional, but working with sources seems sketchy

--- index.js
+++ index.js
@@ -3,59 +3,55 @@ const cors = require('cors');
 const axios = require('axios')
 const getTitleAtUrl = require('get-title-at-url');
 const subjects = require("subject-extractor")
+const NewsAPI = require('newsapi');
+const NEWS_API_KEY = '75ab2037750b4c56825c31ea50160ea0';
+const newsapi = new NewsAPI(NEWS_API_KEY);
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
-let titlesAndLinks = []
-
-const getURL = (keywords, res) => {
-    const googleAPI = "https://www.googleapis.com/customsearch/v1"
-    
-    try {
-        axios.get(googleAPI, {
-            params: {
-                key: "AIzaSyBPKUH3mZ6cTutHvO3ID2G42lYXkjoXWzI",
-                cx: "016572307064780577671:dwwtulj6stk",
-                q: keywords
-            }
-        })
-        .then(response => {
-            if(response.data) {
-                response.data.items.forEach(element => {
-                    titlesAndLinks.push({
-                        title: element.title,
-                        link: element.link
-                    })
-                })
-            }
-            res.status(200).send(titlesAndLinks)
-            titlesAndLinks=[]
-        })
-    } catch (err) {
-        console.log(err)
+//all helper functions are placed here within this class
+class helperFunctions {
+    static getKeywords = (title, res) => {
+        console.log("After title", title)
+        let keywords = ""
+        keywords = subjects.extract(title)
+        console.log(keywords)
+        helperFunctions.getURL(keywords, res)
     }
-}
+    static getURL = (keywords, res) => {
+        newsapi.v2.everything({
+            q: keywords,
+            language: 'en',
+            sortBy: 'relevancy',
+            page: 1
+        }).then(response => {
+            console.log(response);
+            let articles = response.articles;
+            let links = []
+            articles.forEach (element => {
+                links.push({
+                    title: element.title,
+                    link : element.url
+                })
+            })
+            res.status(200).send(links);
+            
+        })
+    }
+    static getTitle = (url, res) => {
+        return Promise.resolve(getTitleAtUrl(url, function(title){
+            console.log(title);
+            helperFunctions.getKeywords(title, res)
+        }))
+    }
+};
 
-const getKeywords = (title, res) => {
-    console.log("After title", title)
-    let keywords = ""
-    subjects.extractAll(title).forEach(element => {
-        keywords+=element
-        keywords+=" "
-    })
-    console.log(keywords)
-
-    getURL(keywords, res)
-}
-
-const getTitle = (url, res) => {
-    return Promise.resolve(getTitleAtUrl(url, function(title){
-        console.log(title);
-        getKeywords(title, res)
-    }))
+//this is the finall call that sends results!
+result = async (url, res) => {
+    await helperFunctions.getTitle(url,res)
 }
 
 app.get('/opposite', async (req, res) => {
@@ -64,12 +60,13 @@ app.get('/opposite', async (req, res) => {
     console.log('request received', url)
 
     try {
-        getTitle(url, res)
+        await result(url, res)
     } catch (err) {
         console.log(err)
     }
     
 })
+
 
 app.listen(9000, () => {
     console.log('Listening on Port 9000')
