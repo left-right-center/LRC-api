add188813931ca8d6cfbe608c948e1a898e193c2
Sanya <purvi.sanya@gmail.com>
2020-08-19 15:57:25 +0400

    refactor code

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const axios = require('axios')
 const getTitleAtUrl = require('get-title-at-url');
 const subjects = require("subject-extractor")
 const NewsAPI = require('newsapi');
+const subject = require('subject-extractor');
+
 const NEWS_API_KEY = '75ab2037750b4c56825c31ea50160ea0';
 const newsapi = new NewsAPI(NEWS_API_KEY);
 
@@ -12,62 +14,160 @@ app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
-//all helper functions are placed here within this class
-class helperFunctions {
-    static getKeywords = (title, res) => {
-        console.log("After title", title)
-        let keywords = ""
-        keywords = subjects.extract(title)
-        console.log(keywords)
-        helperFunctions.getURL(keywords, res)
-    }
-    static getURL = (keywords, res) => {
-        newsapi.v2.everything({
-            q: keywords,
+const getTitle = async (url) => {
+    return new Promise((resolve, reject) => {
+        getTitleAtUrl(url, function(title) {
+            resolve(title)
+        })
+    })
+}
+
+const getLinks = async (keyword) => {
+    const promise = new Promise( async (resolve, reject) => {
+
+        const response = await newsapi.v2.everything({
+            q: keyword,
             language: 'en',
             sortBy: 'relevancy',
             page: 1
-        }).then(response => {
-            console.log(response);
-            let articles = response.articles;
-            let links = []
-            articles.forEach (element => {
-                links.push({
-                    title: element.title,
-                    link : element.url
-                })
-            })
-            res.status(200).send(links);
-            
         })
-    }
-    static getTitle = (url, res) => {
-        return Promise.resolve(getTitleAtUrl(url, function(title){
-            console.log(title);
-            helperFunctions.getKeywords(title, res)
-        }))
-    }
-};
 
-//this is the finall call that sends results!
-result = async (url, res) => {
-    await helperFunctions.getTitle(url,res)
+        if(response === undefined) {
+            reject('newsapi failed to return response')
+        }
+
+        let articles = response.articles
+        let links = []
+        articles.forEach( element => {
+            links.push({
+                title: element.title,
+                link: element.url
+            })
+        })
+        resolve(links)
+    })
+    return promise;
 }
 
-app.get('/opposite', async (req, res) => {
+app.get('/links', async (req, res) => {
     const { url } = req.body
-    console.log(1)
-    console.log('request received', url)
+    console.log('Request received')
 
     try {
-        await result(url, res)
+        const title = await getTitle(url)
+        if(title === undefined) {
+            console.log('An error occured', error.message)
+            return res.status(404).send(error.message)
+        }
+        let keyword = subject.extract(title)
+        const links = await getLinks(keyword)
+        if(links !== undefined) {
+            return res.status(200).send(links)
+        } else {
+            console.log('an error occurred', error.message)
+            return res.status(404).send(error.message)
+        }
+        
     } catch (err) {
-        console.log(err)
+        return res.status(404).send(err.message)
     }
-    
 })
-
 
 app.listen(9000, () => {
     console.log('Listening on Port 9000')
 })
+
+// //all helper functions are placed here within this class
+// class helperFunctions {
+//     static getKeywords = (title, res) => {
+//         console.log("After title", title)
+//         let keywords = ""
+//         keywords = subjects.extract(title)
+//         console.log(keywords)
+//         helperFunctions.getURL(keywords, res)
+//     }
+//     static getURL = (keywords, res) => {
+//         newsapi.v2.everything({
+//             q: keywords,
+//             language: 'en',
+//             sortBy: 'relevancy',
+//             page: 1
+//         }).then(response => {
+//             console.log(response);
+//             let articles = response.articles;
+//             let links = []
+//             articles.forEach (element => {
+//                 links.push({
+//                     title: element.title,
+//                     link : element.url
+//                 })
+//             })
+//             res.status(200).send(links);
+            
+//         })
+//     }
+//     static getTitle = (url, res) => {
+//         return Promise.resolve(getTitleAtUrl(url, function(title){
+//             console.log(title);
+//             helperFunctions.getKeywords(title, res)
+//         }))
+//     }
+// };
+
+// //this is the finall call that sends results!
+// result = async (url, res) => {
+//     await helperFunctions.getTitle(url,res)
+// }
+
+// app.get('/opposite', async (req, res) => {
+//     const { url } = req.body
+//     console.log(1)
+//     console.log('request received', url)
+
+//     try {
+//         await result(url, res)
+//     } catch (err) {
+//         console.log(err)
+//     }
+    
+// })
+///////////////////////////////
+// newsapi.v2.everything({
+//     q: keyword,
+//     language: 'en',
+//     sortBy: 'relevancy',
+//     page: 1
+// })
+// .then(response => {
+//     if(response) {
+//         let articles = response.articles
+//         let links = []
+//         articles.forEach( element => {
+//             links.push({
+//                 title: element.title,
+//                 link: element.url
+//             })
+//         })
+//         resolve(links)
+//     } else {
+//         reject('newsapi failed to return response')
+//     }
+// })
+/////////////////////
+// getTitle(url)
+// .then(title => {
+//     let keyword = subject.extract(title) 
+
+//     getLinks(keyword)
+//     .then(links => {
+//         return res.status(200).send(links)
+//     })
+//     .catch(error => {
+//         console.log('an error occurred', error.message)
+//         return res.status(404).send(error.message)
+//     })
+// })
+// .catch(error => {
+//     console.log('An error occured', error.message)
+//     return res.status(404).send(error.message)
+// })
